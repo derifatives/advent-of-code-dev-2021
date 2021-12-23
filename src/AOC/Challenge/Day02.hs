ffe62c5d1b8f51ae786496c89ccb2de4cb4a8ac7
Rif A. Saurous <derifatives@gmail.com>
2021-12-23 09:18:51 -0800

    Solved Day 2.

--- src/AOC/Challenge/Day02.hs
+++ src/AOC/Challenge/Day02.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day02
 -- License     : BSD3
@@ -22,22 +19,48 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day02 (
-    -- day02a
-  -- , day02b
+  day02a
+  , day02b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(MkSol), sParse, sShow, sSolve)
 
-day02a :: _ :~> _
+data Inst = Up Int | Down Int | Forward Int deriving stock (Show)
+data Pos = Pos Int Int Int deriving stock (Show)
+
+parseInst :: [String] -> Inst
+parseInst (i:n:[]) =
+  case i of
+    "up"      -> Up ni
+    "down"    -> Down ni
+    "forward" -> Forward ni
+    _         -> error "Unknown instruction."
+  where ni = read n
+parseInst _ = error "Instruction with more than two words."
+
+updatePosA :: Pos -> Inst -> Pos
+updatePosA (Pos h d _) (Up n)      = Pos h (d-n) 0
+updatePosA (Pos h d _) (Down n)    = Pos h (d+n) 0
+updatePosA (Pos h d _) (Forward n) = Pos (h+n) d 0
+
+updatePosB :: Pos -> Inst -> Pos
+updatePosB (Pos h d a) (Up n)      = Pos h d (a-n)
+updatePosB (Pos h d a) (Down n)    = Pos h d (a+n)
+updatePosB (Pos h d a) (Forward n) = Pos (h+n) (d+a*n) a
+
+posProduct :: Pos -> Int
+posProduct (Pos h d _) = h * d
+
+day02a :: [Inst] :~> Pos
 day02a = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
+    { sParse = Just . map (parseInst . words) . lines
+    , sShow  = show . posProduct
+    , sSolve = Just . foldl updatePosA (Pos 0 0 0)
     }
 
-day02b :: _ :~> _
+day02b :: [Inst] :~> Pos
 day02b = MkSol
-    { sParse = Just
-    , sShow  = show
-    , sSolve = Just
+    { sParse = Just . map (parseInst . words) . lines
+    , sShow  = show . posProduct
+    , sSolve = Just . foldl updatePosB (Pos 0 0 0)
     }
