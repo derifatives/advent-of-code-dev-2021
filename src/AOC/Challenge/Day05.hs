d2b353437377d8e4b32c85c33d1dd5ee2df7fb3d
Rif A. Saurous <derivatives@gmail.com>
2022-03-13 11:09:01 -0700

    Added Day05

--- src/AOC/Challenge/Day05.hs
+++ src/AOC/Challenge/Day05.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day05
 -- License     : BSD3
@@ -9,35 +6,70 @@
 -- Portability : non-portable
 --
 -- Day 5.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day05 (
-    -- day05a
-  -- , day05b
+  day05a
+  , day05b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(MkSol), sParse, sShow, sSolve)
 
-day05a :: _ :~> _
+import Data.List.Split(splitOn)
+import qualified Data.Map as Map
+
+type Pos = (Int, Int)
+type Vent = (Pos, Pos)
+
+type CountMap a = Map.Map a Int
+type VentMap = CountMap Pos
+
+insert :: Ord a => (CountMap a) -> a -> (CountMap a)
+insert cm a = Map.insertWith (+) a 1 cm
+
+parser :: String -> Maybe ([Vent])
+parser input =
+  let lls = lines input
+      poslines = map (splitOn " -> ") lls
+      vents = map readVent poslines
+  in Just vents
+  where readVent posline =
+          let poses = map parsePos posline
+          in (head poses, head (tail poses))
+          
+parsePos :: String -> Pos
+parsePos p =
+  let ns = map read (splitOn "," p)
+  in (head ns, head (tail ns))
+
+notDiagonal :: Vent -> Bool
+notDiagonal ((x1, y1), (x2, y2)) = x1 == x2 || y1 == y2
+
+ventToPositions :: Vent -> [Pos]
+ventToPositions ((x1, y1), (x2, y2))
+  | x1 == x2 = map (\y -> (x1, y)) (range y1 y2)
+  | y1 == y2 = map (\x -> (x, y1)) (range x1 x2)
+  | otherwise = zip (range x1 x2) (range y1 y2)
+  where range a b = if a < b then [a..b] else reverse [b..a]
+
+addPositions :: [Pos] -> VentMap -> VentMap
+addPositions ps vm = foldr (flip insert) vm ps
+
+buildVentMap :: [Vent] -> VentMap
+buildVentMap vents = foldr addPositions Map.empty (map ventToPositions vents)
+
+dangerousPoints :: VentMap -> [Pos]
+dangerousPoints = map fst . filter ((>= 2) . snd) . (Map.assocs)
+
+day05a :: [Vent] :~> Int
 day05a = MkSol
-    { sParse = Just
+    { sParse = (fmap (filter notDiagonal)) . parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = (\ps -> Just (length ps)) . dangerousPoints . buildVentMap 
     }
 
-day05b :: _ :~> _
+day05b :: [Vent] :~> Int
 day05b = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = (\ps -> Just (length ps)) . dangerousPoints . buildVentMap
     }
