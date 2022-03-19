53e23190819817797717f8b2d3057002c265f655
Rif A. Saurous <derivatives@gmail.com>
2022-03-18 21:47:35 -0700

    A bigt of Day06 cleanup.

--- src/AOC/Challenge/Day06.hs
+++ src/AOC/Challenge/Day06.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day06
 -- License     : BSD3
@@ -9,35 +6,46 @@
 -- Portability : non-portable
 --
 -- Day 6.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day06 (
-    -- day06a
-  -- , day06b
+  day06a
+  , day06b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(MkSol), sParse, sShow, sSolve)
 
-day06a :: _ :~> _
+import Data.List.Split(splitOn)
+import qualified Data.Map as Map
+
+type CountMap a = Map.Map a Integer
+type FishMap = CountMap Integer
+
+insert :: Ord a => (CountMap a) -> a -> (CountMap a)
+insert cm a = Map.insertWith (+) a 1 cm
+
+parser :: String -> Maybe FishMap
+parser input = 
+  Just $ foldr (flip insert) Map.empty (map read (splitOn "," input))
+
+runOneDay :: FishMap -> FishMap
+runOneDay fm = foldr (\k -> Map.insert k (newCount k)) Map.empty [0..8]
+  where lk k = Map.findWithDefault 0 k fm
+        newCount k
+          | k <= 5 || k == 7 = lk (k + 1)
+          | k == 6           = (lk 0) + (lk 7)
+          | k == 8           = lk 0
+          | otherwise        = error "Unexpected key."
+
+day06a :: FishMap :~> Integer
 day06a = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . Map.elems . (!! 80) . iterate runOneDay 
     }
 
-day06b :: _ :~> _
+day06b :: FishMap :~> Integer
 day06b = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . Map.elems . (!! 256) . iterate runOneDay 
     }
