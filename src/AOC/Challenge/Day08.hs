f7ef0040944e6814c88cbf8a91a570aeb3f4b085
Rif A. Saurous <derivatives@gmail.com>
2022-03-22 08:13:34 -0700

    Simplified code, especially using fromJust.

--- src/AOC/Challenge/Day08.hs
+++ src/AOC/Challenge/Day08.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day08
 -- License     : BSD3
@@ -9,35 +6,83 @@
 -- Portability : non-portable
 --
 -- Day 8.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day08 (
-    -- day08a
-  -- , day08b
+  day08a
+  , day08b
   ) where
 
 import           AOC.Prelude
+import Data.List(permutations)
+import Data.List.Split(splitOn)
+-- import Data.Maybe(fromJust)
+import Data.Sort(sort)
 
-day08a :: _ :~> _
+toTuple :: [a] -> (a, a)
+toTuple [a1, a2] = (a1, a2)
+toTuple _ = error "Not a 2-list"
+
+parser :: String -> Maybe [([String], [String])]
+parser = Just . map (toTuple . map (map sort . splitOn " ") . (splitOn " | ")) . lines
+
+isUnique :: String -> Bool
+isUnique s = n == 2 || n == 3 || n == 4 || n == 7
+  where n = length s
+
+segmentToDigit :: [(String, Int)]
+segmentToDigit = [("abcefg", 0),
+                  ("cf", 1),
+                  ("acdeg", 2),
+                  ("acdfg", 3),
+                  ("bcdf", 4),
+                  ("abdfg", 5),
+                  ("abdefg", 6),
+                  ("acf", 7),
+                  ("abcdefg", 8),
+                  ("abcdfg", 9)]
+
+patterns :: [String]
+patterns = map fst segmentToDigit
+
+mappings :: [[(Char, Char)]]
+mappings = map (flip zip $ "abcdefg") (permutations "abcdefg")
+
+applyAssocList :: Eq a => [(a, b)] -> [a] -> [b]
+applyAssocList assocs as = map applyAssoc as
+  where applyAssoc a = snd $ fromJust $ find ((== a) . fst) assocs
+
+mappingIsValid :: [(Char, Char)] -> [String] -> Bool
+mappingIsValid _ [] = True
+mappingIsValid assocs (a:as) =
+  elem (sort (applyAssocList assocs a)) patterns && mappingIsValid assocs as
+
+applyValidMapping :: [(Char, Char)] -> [String] -> [Int]
+applyValidMapping mapping ws =
+  let translated = map (sort . applyAssocList mapping) ws
+  in applyAssocList segmentToDigit translated
+
+getValidMapping :: [String] -> [(Char, Char)]
+getValidMapping ws = fromJust $ find (\m -> mappingIsValid m ws) mappings
+
+assembleBase10 :: [Int] -> Int
+assembleBase10 = foldl (\s d -> 10*s + d) 0
+
+subOne :: ([String], [String]) -> Int
+subOne (ms, ds) = assembleBase10 $ applyValidMapping (getValidMapping ms) ds
+
+solveB :: [([String], [String])] -> Int
+solveB = sum . map subOne
+  
+day08a :: [([String], [String])] :~> Int
 day08a = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . map (length . (filter isUnique) . snd)
     }
 
-day08b :: _ :~> _
+day08b :: [([String], [String])] :~> Int
 day08b = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . solveB 
     }
