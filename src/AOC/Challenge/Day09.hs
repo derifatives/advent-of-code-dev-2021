1ee9b2cc8666c0914e5312bd634b5375d24cdb1e
Rif A. Saurous <derivatives@gmail.com>
2022-03-23 21:16:38 -0700

    First working version of Day09.

--- src/AOC/Challenge/Day09.hs
+++ src/AOC/Challenge/Day09.hs
@@ -22,22 +22,61 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day09 (
-    -- day09a
-  -- , day09b
+  day09a
+  , day09b
   ) where
 
 import           AOC.Prelude
+import Data.Char(digitToInt)
+import Data.Array
+import Data.List.Split(splitOn)
+import Data.Sort(sort)
 
-day09a :: _ :~> _
+type Pos = (Int, Int)
+type Heightmap = Array Pos Int
+type Usedmap = Array Pos Bool
+
+parser :: String -> Maybe Heightmap
+parser input = Just $ listArray ((1, 1),(num_rows, num_columns)) elts
+  where lls = map (map digitToInt) $ lines input
+        num_rows = length lls
+        num_columns = length (head lls)
+        elts = concat lls
+
+neighbors :: Heightmap -> Pos -> [Pos]
+neighbors h (r, c) = filter (inRange (bounds h)) [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
+
+lowPointP :: Heightmap -> Pos -> Bool
+lowPointP h i = all (\n -> h ! i < h ! n) (neighbors h i)
+
+totalRisk :: Heightmap -> Int
+totalRisk h = sum $ map (\i -> 1 + h ! i) $ filter (lowPointP h) $ indices h
+
+buildBasin :: Heightmap -> Usedmap -> [Pos] -> Int -> (Int, Usedmap)
+buildBasin _ u [] i = (i, u)
+buildBasin h u (p:ps) i =
+  if usable p
+  then buildBasin h (u // [(p, True)]) ((neighbors h p) ++ ps) (i+1)
+  else buildBasin h u ps i 
+  where usable p' = h ! p' < 9 && u ! p' == False
+
+buildBasins :: Heightmap -> [Int]
+buildBasins h = fst $ foldr buildOne initial (indices h)
+  where initial = ([], listArray (bounds h) (repeat False))
+        buildOne p (bs, u) =
+          let (b, u') = buildBasin h u [p] 0
+          in if b > 0 then (b:bs, u') else (bs, u')
+
+day09a :: Heightmap :~> Int
 day09a = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . totalRisk
     }
 
-day09b :: _ :~> _
+day09b :: Heightmap :~> Int
 day09b = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . product . take 3 . reverse . sort . buildBasins 
     }
