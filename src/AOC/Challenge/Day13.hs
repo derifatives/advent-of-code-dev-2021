1d4c562159732c7837cdbc0f1ec56000363b7d22
Rif A. Saurous <derivatives@gmail.com>
2022-05-02 16:53:28 -0700

    Cleaned up Day13.

--- src/AOC/Challenge/Day13.hs
+++ src/AOC/Challenge/Day13.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day13
 -- License     : BSD3
@@ -9,35 +6,55 @@
 -- Portability : non-portable
 --
 -- Day 13.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day13 (
-    -- day13a
-  -- , day13b
+  day13a
+  , day13b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(MkSol), sParse, sShow, sSolve)
+import Data.List.Split (splitOn)
+import qualified Data.Set as S
+
+type Point = (Int, Int)
+type Points = S.Set Point
+
+data Instruction = X Int | Y Int
+
+parser :: String -> Maybe (Points, [Instruction])
+parser input =
+  let elts = splitOn "\n\n" input
+      points = S.fromList $ map (\i -> read ("(" ++ i ++ ")") :: Point) (lines (head elts))
+      instructions = map readInstructions (lines (elts !! 1))
+  in Just (points, instructions)
+  where
+    readInstructions l =
+      let w = splitOn "=" (words l !! 2)
+          value = read (w !! 1)
+      in if head w == "x" then X value else Y value
+
+foldPoint :: Instruction -> Point -> Point
+foldPoint (X i) (x, y) = (min x (2*i-x), y)
+foldPoint (Y i) (x, y) = (x, min y (2*i-y))
+
+printPoints :: Points -> String
+printPoints p =
+  let maxvals = foldl (\(xn, yn) (x, y) -> (max xn x, max yn y)) (0, 0) (S.toList p)
+  in unlines [[if S.member (x, y) p then '0' else ' ' | x <- [0..(fst maxvals)]] | y <- [0..(snd maxvals)]]
+  
+processFold :: Points -> Instruction -> Points
+processFold = flip (S.map . foldPoint)
 
 day13a :: _ :~> _
 day13a = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \(ps, is) -> Just $ length $ processFold ps (head is)
     }
 
 day13b :: _ :~> _
 day13b = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = \(ps, is) -> Just $ printPoints $ foldl processFold ps is
     }
