caeb882c3b7d7d3de71d4cdf9903b4f4f178764e
Rif A. Saurous <rif@google.com>
2022-07-05 16:53:19 -0400

    Day15

--- src/AOC/Challenge/Day15.hs
+++ src/AOC/Challenge/Day15.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day15
 -- License     : BSD3
@@ -9,35 +6,113 @@
 -- Portability : non-portable
 --
 -- Day 15.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day15 (
-    -- day15a
-  -- , day15b
+  day15a
+  , day15b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(MkSol), sParse, sShow, sSolve)
+import Data.Char(digitToInt)
+import qualified Data.Array.Unboxed as A
+import qualified Data.Set as S
 
-day15a :: _ :~> _
+import qualified Math.Geometry.Grid as G
+import Math.Geometry.Grid.Square(RectSquareGrid, rectSquareGrid)
+-- The Grid package seems to use an unusual / surprising layout convention.
+-- In particular, rectSquareGrid is documented to take "rows" and "columns"
+-- as arguments, but then when indexing it seems to have the column as the first
+-- entry of the index?
+
+type Ix = G.Index RectSquareGrid
+
+data SearchState = SearchState {
+   -- (Min Cost, Actual Cost, Index)
+   active :: S.Set (Int, Int, Ix),
+   used :: S.Set Ix } deriving stock (Show)
+
+class Cave c where
+  cost :: c -> Ix -> Int
+  neighbours :: c -> Ix -> [Ix]
+  size :: c -> (Int, Int)
+
+data SmallCavern = SmallCavern {
+  grid :: RectSquareGrid,
+  array :: A.Array Ix Int } deriving stock (Show)
+
+instance Cave SmallCavern where
+  cost SmallCavern {array=a} ix = a A.! ix
+  size SmallCavern {grid=g} = G.size g
+  neighbours SmallCavern {grid=g} i = G.neighbours g i
+
+data LargeCavern = LargeCavern {
+  baseSize :: (Int, Int),
+  fullGrid :: RectSquareGrid,
+  baseArray :: A.Array Ix Int } deriving stock (Show)
+
+instance Cave LargeCavern where
+  size LargeCavern {fullGrid=g} = G.size g
+  neighbours LargeCavern {fullGrid=g} i = G.neighbours g i
+  cost LargeCavern {baseSize=(rb, cb), baseArray=a} (r, c) =
+    let (rq, rr) = r `quotRem` rb
+        (cq, cr) = c `quotRem` cb
+    in ((((a A.! (rr, cr)) - 1) + rq + cq) `mod` 9) + 1
+      
+buildInitialState :: SearchState
+buildInitialState = SearchState { active = S.singleton (0, 0, (0, 0)),
+                                  used = S.empty }
+
+parser :: String -> Maybe (SmallCavern, SearchState)
+parser s =
+  let lls = lines s
+      rows = length lls
+      columns = length (head lls)
+      grid = rectSquareGrid columns rows
+      elts = [ digitToInt c | l <- lls, c <- l ]
+  in Just $ (SmallCavern { grid=grid,
+                           array=A.listArray ((0, 0), (rows-1, columns-1)) elts },
+             buildInitialState)
+
+findAndRemoveMinCostNode :: SearchState -> ((Int, Int, Ix), SearchState)
+findAndRemoveMinCostNode SearchState {active=a, used=u} =
+  let v@(minCost, currentCost, ix) = S.findMin a
+  in ((minCost, currentCost, ix), SearchState { active=S.delete v a, used=S.insert ix u })
+            
+exploreFromNode :: (Cave cv) => cv -> Int-> SearchState -> Ix -> SearchState
+exploreFromNode cave currentCost ss ix = foldr f ss (neighbours cave ix)
+  where f ix'@(r, c) ss'@SearchState {active=a, used=u} =
+          if S.member ix' u
+          then ss'
+          else let new_cost = currentCost + AOC.Challenge.Day15.cost cave ix'
+               in ss' { active=S.insert (new_cost-r-c, new_cost, ix') a }
+
+minDistance :: (Cave c) => (c, SearchState) -> Int
+minDistance (cave, ss) =
+  let ((_, currentCost, ix), new_ss) = findAndRemoveMinCostNode ss
+      (rows, columns) = size cave
+  in if ix == (columns-1, rows-1)
+     then currentCost
+     else minDistance (cave, exploreFromNode cave currentCost new_ss ix)
+
+expandCavern :: (SmallCavern, SearchState) -> (LargeCavern, SearchState)
+expandCavern (SmallCavern {grid=g, array=a}, ss) =
+  let (r, c) = G.size g
+      e = 5
+  in (LargeCavern { baseSize = (r, c),
+                    fullGrid = rectSquareGrid (r*e) (c*e),
+                    baseArray = a },
+      ss)
+
+day15a :: (SmallCavern, SearchState) :~> Int
 day15a = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . minDistance
     }
 
-day15b :: _ :~> _
+day15b :: (SmallCavern, SearchState) :~> Int
 day15b = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . minDistance . expandCavern
     }
