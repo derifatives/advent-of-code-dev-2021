fc511628153ebab0f4c014992e40f8559003fecb
Rif A. Saurous <rif@google.com>
2022-12-05 21:13:59 -0800

    Implemented Day06

--- src/AOC/Challenge/Day06.hs
+++ src/AOC/Challenge/Day06.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day06
 -- License     : BSD3
@@ -9,35 +6,33 @@
 -- Portability : non-portable
 --
 -- Day 6.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day06 (
-    -- day06a
-  -- , day06b
+  day06a
+  , day06b
   ) where
 
 import           AOC.Prelude
 
-day06a :: _ :~> _
+import AOC.Solver ((:~>)(MkSol), sParse, sShow, sSolve)
+import Data.List (nub)
+
+findFirstPositionWithNUnique :: Int -> String -> Int
+findFirstPositionWithNUnique n s =
+  let firstN = take n s
+  in if firstN == nub (firstN) then n else 1 + findFirstPositionWithNUnique n (tail s)
+
+
+day06a :: String :~> Int
 day06a = MkSol
     { sParse = Just
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . findFirstPositionWithNUnique 4
     }
 
-day06b :: _ :~> _
+day06b :: String :~> Int
 day06b = MkSol
     { sParse = Just
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just  . findFirstPositionWithNUnique 14
     }
