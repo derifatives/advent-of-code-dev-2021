af0433fce90685a3e9c9286c4f177c983cedc775
Rif A. Saurous <rif@google.com>
2022-12-09 07:57:43 -0800

    Day 09

--- src/AOC/Challenge/Day09.hs
+++ src/AOC/Challenge/Day09.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day09
 -- License     : BSD3
@@ -9,35 +6,74 @@
 -- Portability : non-portable
 --
 -- Day 9.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day09 (
-    -- day09a
-  -- , day09b
+  day09a
+  , day09b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(MkSol), sParse, sShow, sSolve)
+import Data.Foldable (foldl')
+import qualified Data.Set as S
+import Linear.V2
 
-day09a :: _ :~> _
+type Pos = V2 Int
+type Offset = V2 Int
+type Visited = S.Set Pos
+
+-- data Rope = Rope Pos Pos deriving stock (Show)
+type Rope = [Pos]
+
+wordToOffset :: String -> Offset
+wordToOffset "R" = V2 1 0
+wordToOffset "U" = V2 0 1
+wordToOffset "L" = V2 (-1) 0
+wordToOffset "D" = V2 0 (-1)
+wordToOffset _ = undefined
+
+parser :: String -> Maybe [(Offset, Int)]
+parser = Just . map parseLine . lines
+  where
+    parseLine l = let ws = words l
+      in (wordToOffset $ ws !! 0, read $ ws !! 1)
+
+moveSegment :: Pos -> Pos -> Pos
+moveSegment (V2 hx hy) tl@(V2 tx ty) =
+  case max (abs (hx - tx)) (abs (hy - ty)) of
+    0 -> tl
+    1 -> tl
+    2 -> V2 (tx + signum (hx - tx)) (ty + signum (hy - ty))
+    _ -> undefined
+    
+moveTail :: Rope -> Rope -> Rope
+moveTail [] newReversed = reverse newReversed
+moveTail (t:ts) newReversed =
+  moveTail ts (moveSegment (head newReversed) t:newReversed)
+
+moveRope :: Rope -> Offset -> Rope
+moveRope rope offset = moveTail (tail rope) [head rope + offset]
+
+recordVisit :: (Rope, Visited) -> (Rope, Visited)
+recordVisit (r, v) = (r, S.insert (last r) v)
+ 
+runSequence :: (Rope, Visited) -> (Offset, Int) ->  (Rope, Visited)
+runSequence s (_, 0) = s
+runSequence (r, v) (o, i) =
+  runSequence (recordVisit $ (moveRope r o, v)) (o, i-1)
+  
+initState :: Int -> (Rope, Visited)
+initState n = (take n (repeat $ V2 0 0), S.insert (V2 0 0) S.empty)
+  
+day09a :: [(Offset, Int)] :~> Int
 day09a = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . S.size . snd . foldl' runSequence (initState 2)
     }
 
-day09b :: _ :~> _
+day09b ::  [(Offset, Int)] :~> Int
 day09b = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . S.size . snd . foldl' runSequence (initState 10)
     }
