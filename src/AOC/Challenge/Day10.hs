5e7d4f22a3e1339ae2e1ca03cb60f52a3843492a
Rif A. Saurous <rif@google.com>
2022-12-09 23:16:15 -0800

    Day 10.

--- src/AOC/Challenge/Day10.hs
+++ src/AOC/Challenge/Day10.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day10
 -- License     : BSD3
@@ -9,35 +6,44 @@
 -- Portability : non-portable
 --
 -- Day 10.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day10 (
-    -- day10a
-  -- , day10b
+  day10a
+  , day10b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(MkSol), sParse, sShow, sSolve)
+import Data.List.Split (chunksOf)
 
-day10a :: _ :~> _
+parser :: String -> Maybe [[String]]
+parser = Just . map words . lines
+
+process :: [Int] -> [[String]] -> [Int]
+process ns [] = reverse ns
+process ns@(hn:_) (inst:insts) =
+  case inst of
+    ["noop"] -> process (hn:ns) insts
+    ["addx", nw] -> let nv = hn + (read nw) in process (nv:hn:ns) insts
+    _ -> undefined
+process _ _ = undefined
+
+signalStrengths :: [Int] -> [Int] -> [Int]
+signalStrengths inds ss = map (\i -> i * ss !! i) inds
+
+drawCRT :: [Int] -> [String]
+drawCRT ss = chunksOf 40 $ map posToChar [0..239]
+  where posToChar i = if abs (i `mod` 40 - ss !! (i + 1)) <= 1 then '#' else '.'
+    
+day10a :: [[String]] :~> Int
 day10a = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . signalStrengths [20, 60, 100, 140, 180, 220] . process [1, 0]
     }
 
-day10b :: _ :~> _
+day10b :: [[String]] :~> [String]
 day10b = MkSol
-    { sParse = Just
+    { sParse = parser
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . drawCRT . process [1, 0]
     }
