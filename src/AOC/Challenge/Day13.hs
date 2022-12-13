c5d13942867cf330f015f2604e7b37087ac6b0d1
Rif A. Saurous <rif@google.com>
2022-12-13 06:15:56 -0800

    Implemented Day13.

--- src/AOC/Challenge/Day13.hs
+++ src/AOC/Challenge/Day13.hs
@@ -1,7 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
--- |
 -- Module      : AOC.Challenge.Day13
 -- License     : BSD3
 --
@@ -9,35 +5,59 @@
 -- Portability : non-portable
 --
 -- Day 13.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day13 (
-    -- day13a
-  -- , day13b
+  day13a
+  , day13b
   ) where
 
-import           AOC.Prelude
+import AOC.Solver ((:~>)(MkSol), sParse, sShow, sSolve)
+import Data.Char (isDigit)
+import Data.List (elemIndices, findIndices, sortBy)
+import Data.List.Split (chunksOf)
 
-day13a :: _ :~> _
+pullDigit :: String -> (Int, String)
+pullDigit s =
+  let (di, r) = span isDigit s
+  in (read di, r)
+
+listize :: String -> String
+listize s = let (d, r) = pullDigit s in ('[':show d) ++ (']':r)
+
+matchedP :: String -> String -> Ordering
+matchedP [] [] = undefined
+matchedP "]" "]" = undefined
+matchedP ('[':r1) ('[':r2) = matchedP r1 r2
+matchedP (']':r1) (']':r2) = matchedP r1 r2
+matchedP (',':r1) (',':r2) = matchedP r1 r2
+matchedP (']':_) _ = LT
+matchedP _ (']':_) = GT
+matchedP s1 s2@('[':_) = matchedP (listize s1) s2
+matchedP s1@('[':_) s2 = matchedP s1 (listize s2)
+matchedP s1 s2 =
+  let (d1, r1) = pullDigit s1
+      (d2, r2) = pullDigit s2
+  in case compare d1 d2 of
+    LT -> LT
+    GT -> GT
+    EQ -> matchedP r1 r2
+
+compareChunk :: [String] -> Ordering
+compareChunk lls = matchedP (lls !! 0) (lls !! 1)
+
+day13a :: _ :~> Int
 day13a = MkSol
-    { sParse = Just
+    { sParse = Just . chunksOf 3 . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . sum . map (1+) . elemIndices LT . map compareChunk
     }
 
-day13b :: _ :~> _
+divs :: [String]
+divs = ["[[2]]", "[[6]]"]
+  
+day13b :: _ :~> Int
 day13b = MkSol
-    { sParse = Just
+    { sParse = Just . filter (not . null) . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . product . map (1+) . findIndices (`elem` divs) . sortBy matchedP . (divs ++)
     }
